8f202441f9a7ac5647d5670515f0a14289fde6fa
Nicholas Lee <csudcy@gmail.com>
2015-12-24 09:31:52 +0000

    Added basic CLI handling

--- RGBStrip/__main__.py
+++ RGBStrip/__main__.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-import sys
+import argparse
 
 from RGBStrip import manager
+from RGBStrip import server
 
+# Parse our args
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--server', help='If the web server should be run', action='store_true')
+parser.add_argument('config', help='The config file to load')
+args = parser.parse_args()
 
-if len(sys.argv) != 2:
-    print 'Usage: python -m RGBStrip config_file.yaml'
-    sys.exit(1)
-
-manager = manager.RGBStripManager()
-manager.load_config(sys.argv[1])
-manager.output_forever()
-
-# from RGBStrip import server
-# server.start_server(sys.argv[1])
+# Start
+if args.server:
+    server.start_server(args.config)
+else:
+    manager = manager.RGBStripManager()
+    manager.load_config(args.config)
+    manager.output_forever()
