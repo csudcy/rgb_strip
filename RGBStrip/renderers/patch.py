a07d2d55283e204f57d98380b2f3a74f893340a4
csudcy <csudcy@gmail.com>
2020-11-23 20:10:05 +0000

    Move patch to timed renderer

--- RGBStrip/renderers/patch.py
+++ RGBStrip/renderers/patch.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 from RGBStrip import utils
-from RGBStrip.renderers.base import BaseSingleRenderer
+from RGBStrip.renderers.base import BaseSingleTimedRenderer
 
 
-class PatchRenderer(BaseSingleRenderer):
+class PatchRenderer(BaseSingleTimedRenderer):
 
   def __init__(
       self,
@@ -13,26 +13,19 @@ class PatchRenderer(BaseSingleRenderer):
       section=None,
       palette=None,
       active=True,
-      # TODO: remove delay?
-      delay=1,
-      start_index=0):
+  ):
     super().__init__(loader,
                      name=name,
                      section=section,
                      palette=palette,
                      active=active)
+    self.INDEX = 0
 
-    self.INDEX = start_index % len(self.PALETTE)
-    self.STEP_DELAYED = 0
-    self.STEP_DELAY = delay
-
-  def do_render(self):
+  def do_render_display(self):
     rgb_colour = self.PALETTE[self.INDEX]
     for x in range(self.SECTION.WIDTH):
       for y in range(self.SECTION.HEIGHT):
         self.SECTION.add_led(x, y, rgb_colour)
-    self.STEP_DELAYED += 1
-    if self.STEP_DELAYED >= self.STEP_DELAY:
-      # We have stayed on this step for STEP_DELAY frames; move to the next step
-      self.STEP_DELAYED = 0
+
+  def do_render_step(self):
       self.INDEX = (self.INDEX + 1) % len(self.PALETTE)
