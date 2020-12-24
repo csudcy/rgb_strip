3d256c34cc4b64f0fb2a2840c24bcb3be1396218
csudcy <csudcy@gmail.com>
2020-12-24 13:35:45 +0000

    Make Gif display threaded

--- RGBGifDisplay/rgb_gif_display.py
+++ RGBGifDisplay/rgb_gif_display.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-import traceback
 import os
 import random
 import time
+import traceback
+from threading import Thread
 from typing import List, Tuple
 
 import click
-from PIL import Image
-from luma.core.device import device as LumaDevice
 import PIL
+from luma.core.device import device as LumaDevice
+from PIL import Image
+
+NamedImageType = Tuple[str, Image.Image]
 
 
 @click.group()
@@ -21,9 +24,19 @@ def main() -> None:
 @click.argument('width', type=int)
 @click.argument('height', type=int)
 @click.argument('directory')
-@click.option('--alpha', help='How bright to show the pixels (0-255)', type=int, default=40)
-@click.option('--delay', help='How many ms to delay between frames', type=int, default=0)
-@click.option('--emulate', help='Emulate the display in terminal', type=bool, default=False, is_flag=True)
+@click.option('--alpha',
+              help='How bright to show the pixels (0-255)',
+              type=int,
+              default=40)
+@click.option('--delay',
+              help='How many ms to delay between frames',
+              type=int,
+              default=0)
+@click.option('--emulate',
+              help='Emulate the display in terminal',
+              type=bool,
+              default=False,
+              is_flag=True)
 def run(
     width: int,
     height: int,
@@ -32,68 +45,89 @@ def run(
     delay: int,
     emulate: bool,
 ):
-  images = get_images(width, height, directory)
-  if not images:
-    raise Exception('No files found!')
-
-  device = get_device(width, height, emulate)
-  device.contrast(alpha)
-
-  delay_seconds = delay / 1000.0
-
-  while True:
-    name, image = random.choice(images)
-
-    for frame_index in range(image.n_frames):
-      image.seek(frame_index)
-      resized = image.resize((width, height))
-      converted = resized.convert('RGB')
-      device.display(converted)
-      print(f'{name} : {frame_index} / {image.n_frames}...')
-      time.sleep(delay_seconds)
-
-
-def get_device(width: int, height: int, emulate: bool) -> LumaDevice:
   if emulate:
-    from luma.emulator.device import asciiblock
-    return asciiblock(width=width, height=height)
+    GifDisplayClass = GifDisplayEmulator
   else:
-    from luma.led_matrix.device import ws2812
-    return ws2812(width=width, height=height)
+    GifDisplayClass = GifDisplayLeds
+
+  display_thread = GifDisplayClass(width, height, alpha, delay, directory)
+  display_thread.daemon = True
+  display_thread.start()
+  display_thread.join()
 
 
-def get_images(width: int, height: int, directory: str) -> List[Tuple[str, Image.Image]]:
-  images: List[Tuple[str, Image.Image]] = []
-  for filename in os.listdir(directory):
-    filepath = os.path.join(directory, filename)
+class GifDisplayBase(Thread):
 
-    # Check it's an image
-    print(f'Checking {filename}...')
-    try:
-      image = Image.open(filepath)
-    except PIL.UnidentifiedImageError:
-      print('  Skipped: Not an image')
-      continue
+  def __init__(
+      self,
+      width: int,
+      height: int,
+      alpha: int,
+      delay: int,
+      directory: str,
+  ):
+    super().__init__()
+    self.width = width
+    self.height = height
+    self.device = self._get_device(width, height)
+    self.device.contrast(alpha)
 
-    # Check some things about the image
-    checks = [
-        # (image.mode == 'RGB', 'Must be RGB'),
-        (hasattr(image, 'n_frames'), 'Must be multiple frames'),
-        # (image.width == width, f'Width must be {width} pixels'),
-        # (image.height == height, f'Height must be {height} pixels'),
-    ]
-    failed_checks = [
-        message
-        for passed, message in checks
-        if not passed
-    ]
-    if failed_checks:
-      print(f'  Skipped: {failed_checks}')
-    else:
+    self.delay_seconds = delay / 1000.0
+    self.images = self._get_images(directory)
+
+  def _get_device(self, width: int, height: int):
+    raise Exception('Must be overridden!')
+
+  def _get_images(self, directory: str) -> List[NamedImageType]:
+    images: List[NamedImageType] = []
+    for filename in os.listdir(directory):
+      filepath = os.path.join(directory, filename)
+
+      # Check it's an image
+      print(f'Checking {filename}...')
+      try:
+        image = Image.open(filepath)
+      except PIL.UnidentifiedImageError:
+        print('  Skipped: Not an image')
+        continue
+
+      if not hasattr(image, 'n_frames'):
+        print('  Skipped: Must be multiple frames')
+        continue
+
       print('  Good!')
       images.append((filename, image))
 
-  return images
+    if not images:
+      raise Exception('No files found!')
+
+    return images
+
+  def run(self):
+    while True:
+      name, image = random.choice(self.images)
+
+      for frame_index in range(image.n_frames):
+        image.seek(frame_index)
+        resized = image.resize((self.width, self.height))
+        converted = resized.convert('RGB')
+        self.device.display(converted)
+        print(f'{name} : {frame_index} / {image.n_frames}...')
+        time.sleep(self.delay_seconds)
+
+
+class GifDisplayEmulator(GifDisplayBase):
+
+  def _get_device(self, width: int, height: int):
+    from luma.emulator.device import asciiblock
+    return asciiblock(width=width, height=height)
+
+
+class GifDisplayLeds(GifDisplayBase):
+
+  def _get_device(self, width: int, height: int):
+    from luma.led_matrix.device import ws2812
+    return ws2812(width=width, height=height)
 
 
 if __name__ == "__main__":
