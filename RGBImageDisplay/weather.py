b796459546087000f7b2aa73d3f4887f1f5f7242
Nick <csudcy@gmail.com>
2024-04-28 14:07:26 +0100

    Pull weather forecast

--- RGBImageDisplay/weather.py
+++ RGBImageDisplay/weather.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 from dataclasses import dataclass
-from datetime import timedelta
+import datetime
 import pathlib
 import threading
 from typing import Optional
@@ -10,54 +10,89 @@ import requests_cache
 import retry_requests
 
 
-
 CURRENT_DIRECTORY = pathlib.Path(__file__).parent
 WEATHER_DIRECTORY = CURRENT_DIRECTORY / 'weather'
-"""
-See https://open-meteo.com/en/docs
-0 - Clear sky
-1, 2, 3 - Mainly clear, partly cloudy, and overcast
-45, 48 - Fog and depositing rime fog
-51, 53, 55 - Drizzle: Light, moderate, and dense intensity
-56, 57 - Freezing Drizzle: Light and dense intensity
-61, 63, 65 - Rain: Slight, moderate and heavy intensity
-66, 67 - Freezing Rain: Light and heavy intensity
-71, 73, 75 - Snow fall: Slight, moderate, and heavy intensity
-77 - Snow grains
-80, 81, 82 - Rain showers: Slight, moderate, and violent
-85, 86 - Snow showers slight and heavy
-95 - Thunderstorm: Slight or moderate
-96, 99 - Thunderstorm with slight and heavy hail
-"""
-WEATHER_CODE_MAP = {}
 
+FORECAST_API = 'https://api.open-meteo.com/v1/forecast'
+
+# Weather codes from: https://open-meteo.com/en/docs
+WEATHER_ICON_BY_CODE = {
+    # 0 - Clear sky
+    0: 'sun',
+    # 1, 2, 3 - Mainly clear, partly cloudy, and overcast
+    1: 'sun',
+    2: 'sun_cloud',
+    3: 'cloud',
+    # 45, 48 - Fog and depositing rime fog
+    45: 'sun_cloud',
+    48: 'sun_cloud',
+    # 51, 53, 55 - Drizzle: Light, moderate, and dense intensity
+    51: 'sun_rain',
+    53: 'sun_rain',
+    55: 'sun_rain',
+    # 56, 57 - Freezing Drizzle: Light and dense intensity
+    56: 'freeze',
+    57: 'freeze',
+    # 61, 63, 65 - Rain: Slight, moderate and heavy intensity
+    61: 'rain',
+    63: 'rain',
+    65: 'rain',
+    # 66, 67 - Freezing Rain: Light and heavy intensity
+    66: 'freeze',
+    67: 'freeze',
+    # 71, 73, 75 - Snow fall: Slight, moderate, and heavy intensity
+    71: 'snow',
+    73: 'snow',
+    75: 'snow',
+    # 77 - Snow grains
+    77: 'snow',
+    # 80, 81, 82 - Rain showers: Slight, moderate, and violent
+    80: 'rain',
+    81: 'rain',
+    82: 'storm',
+    # 85, 86 - Snow showers slight and heavy
+    85: 'snow',
+    86: 'snow',
+    # 95 - Thunderstorm: Slight or moderate
+    95: 'storm',
+    # 96, 99 - Thunderstorm with slight and heavy hail
+    96: 'storm',
+    99: 'storm',
+}
 
 
 @dataclass
-class ImageInfo:
-  parent: str
+class ForecastData:
+  start_time: datetime.datetime
+  end_time: datetime.datetime
+  temperature: float
+  weather_code: int
+  weather_icon: Optional[str]
 
 
 class WeatherService(threading.Thread):
 
-  def __init__(self):
-    # Make an open-meteo session
+  def __init__(
+      self,
+      *,
+      latitude: float,
+      longitude: float,
+  ):
+    # Make a session for getting open-meteo data
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
     self.session = retry_requests.retry(cache_session,
                                         retries=5,
                                         backoff_factor=0.2)
-
-  # TODO: Cache this!
-  def _fetch_weather(self) -> Optional[str]:
-    url = 'https://api.open-meteo.com/v1/forecast'
-    params = {
-        # TODO: Get lat/lng from CLI args
-        'latitude': 52.52,
-        'longitude': 13.41,
+    self.params = {
+        'latitude': latitude,
+        'longitude': longitude,
         'hourly': ['temperature_2m', 'weather_code'],
         'forecast_days': 1
     }
-    response = self.session.get(url, params=params)
+
+  # TODO: Cache this!
+  def _fetch_forecast(self) -> list[ForecastData]:
+    response = self.session.get(FORECAST_API, params=self.params)
     response.raise_for_status()
     response_json = response.json()
     """
@@ -95,20 +130,36 @@ class WeatherService(threading.Thread):
     """
 
     hourly = response_json['hourly']
-    start_times = hourly['time']
+    start_times = [datetime.datetime.fromisoformat(iso_time) for iso_time in hourly['time']]
     temperatures = hourly['temperature_2m']
     weather_codes = hourly['weather_code']
     combined = zip(start_times, temperatures, weather_codes)
 
-    one_hour = timedelta(hours=1)
+    one_hour = datetime.timedelta(hours=1)
 
-    for start_time, temperature, weather_code in combined:
-      print((start_time, temperature, weather_code))
+    return [
+        ForecastData(
+            start_time=start_time,
+            end_time=start_time + one_hour,
+            temperature=temperature,
+            weather_code=weather_code,
+            weather_icon=WEATHER_ICON_BY_CODE.get(weather_code),
+        )
+        for start_time, temperature, weather_code in combined
+    ]
 
-    import pdb
-    pdb.set_trace()
+  def get_current_forecast(self) -> Optional[ForecastData]:
+    # TODO: Move this to a threaded timer
+    forecast = self._fetch_forecast()
+
+    now = datetime.datetime.now()
+    for forecast_data in forecast:
+      if forecast_data.start_time <= now <= forecast_data.end_time:
+        return forecast_data
 
     return None
 
-  def _get_current_weather_icon(self) -> Optional[str]:
-    pass
+
+if __name__ == '__main__':
+  weather = WeatherService(latitude=52.52, longitude=13.41)
+  print(weather.get_current_forecast())
